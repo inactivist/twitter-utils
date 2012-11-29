e2cc05a7a5eebe5597c594fb10b762a8c7422356
inactivist <inactivist@gmail.com>
2012-11-28 16:49:23 -0800

    Now uses requests-cache module if available, and caches successful requests for one minute.

--- twitter-utils/search-tweets.py
+++ twitter-utils/search-tweets.py
@@ -13,7 +13,18 @@ import requests
 import requests.auth
 import simplejson as json
 
+try:
+    CACHE_NAME = __file__ + "-cache"
+    # Use requests-cache if available.  We cache successful
+    # requests for 1 minute, because requests_cache only supports
+    # cache duration multiples of a minute...
+    import requests_cache
+    requests_cache.configure(cache_name=CACHE_NAME, expire_after=1)
+except ImportError:
+    pass
+
 def twitter_oauth():
+    """Return OAUTH authentication to be used in by requests library."""
     # Must supply twitter account credentials in environment variables.
     consumer_key = unicode(os.environ['CONSUMER_KEY'])
     consumer_secret = unicode(os.environ['CONSUMER_SECRET'])
@@ -34,8 +45,19 @@ API_INFO = {
     "1.1": { "url": "https://api.twitter.com/1.1/search/tweets.json", "auth": twitter_oauth }
 }
 
-def twitter_date(value):
-    return value
+# Tuple of twitter search args and various attributes.
+TWITTER_OPTS = (
+    {'name': 'since_id'},
+    {'name': 'max_id'},
+    {'name': 'geocode'},
+    {'name': 'lang'},
+    {'name': 'locale'},
+    {'name': 'result_type'},
+    {'name': 'count', 'type': int}, # count not supported in 1.0 API.
+    {'name': 'until' },
+    {'name': 'include_entities', 'action': 'store_true', 'default': True },
+    {'name': 'rpp', 'type': int}  # rpp is not supported in 1.1 API.
+)
 
 def hyphenize(name):
     """Convert underscores to hyphens."""
@@ -49,23 +71,18 @@ def api_version_type(value):
         raise argparse.ArgumentTypeError("must be one of %s" % API_INFO.keys())
     return value
 
+def is_twitter_arg(name):
+    """Return TRUE if name is a Twitter search argument."""
+    return name in [x.get('name') for x in TWITTER_OPTS]
+
+
 def get_opts():
+    """Parse command line arguments and return them as argsparse options."""
+    
     #
     # List of acceptable options.
     # If entry is a string, the argument is a string type representing the GET search API parameter name.
     # Otherwise, it's a dict with various parameters.
-    easy_opts = (
-        'since_id',
-        'geocode',
-        'lang',
-        'locale',
-        'result_type',
-        {'name': 'count', 'type': int},
-        {'name': 'until', 'type': twitter_date},
-        {'name': 'max_id', 'type': int},
-        {'name': 'include_entities', 'action': 'store_true', 'default': True },
-        {'name': 'rpp', 'type': int}  # rpp is not supported in 1.1 API.
-    )
     parser = argparse.ArgumentParser(description="Query Twitter's GET Search API using Tweepy and output results as JSON strings.")
     # Program args
     my_args = parser.add_argument_group("Program options")
@@ -75,51 +92,51 @@ def get_opts():
                          help="Twitter API version (default=1)",
                          metavar='version')
     
-    # Twitter args
+    # Twitter-specific args
     twitter_args = parser.add_argument_group('Twitter Search args')
     twitter_args.add_argument("q", help="The Get Search 'q' value.", metavar='query')
     
-    for opt in easy_opts:
-        if isinstance(opt, str):
-            twitter_args.add_argument("--%s" % hyphenize(opt),
-                                help=make_help_text(opt),
-                                metavar=opt)
-        elif isinstance(opt, dict):
-            # 'name' is mandatory.
-            name = opt['name']
-            a = {
-                    'help': make_help_text(name),
-                }
-            if opt.get('default'): a['default'] = opt.get('default')
-            if opt.get('type'): a['type'] = opt.get('type')
-            if opt.get('action'): a['action'] = opt.get('action')
-            # metavar is not allowed with action='store_true'
-            if opt.get('action') != 'store_true': a['metavar'] = name
-            
-            twitter_args.add_argument("--%s" % hyphenize(name), **a)
-        else:
-            raise ValueError(opt)
+    for opt in TWITTER_OPTS:
+        # 'name' is mandatory.
+        name = opt['name']
+        a = {
+                'help': make_help_text(name),
+            }
+        if opt.get('default'): a['default'] = opt.get('default')
+        if opt.get('type'): a['type'] = opt.get('type')
+        if opt.get('action'): a['action'] = opt.get('action')
+        # metavar is not allowed with action='store_true'
+        if opt.get('action') != 'store_true': a['metavar'] = name
+        # TODO: If API != "1", convert 'rpp' to 'count'
+        # TODO: Deal with negation of --include-entities...
+        twitter_args.add_argument("--%s" % hyphenize(name), **a)
     
     return parser.parse_args()
 
 opts = get_opts()
+
 # config = {'verbose': sys.stdout}
 config = {}
-params = dict([v for v in vars(opts).iteritems() if v[1] is not None])
+
+# Get relevant parameters...
+params = dict([v for v in vars(opts).iteritems() if v[1] is not None and is_twitter_arg(v[0])])
+params['q'] = opts.q
 
 request_args = {
     'params': params,
     'config': config
 }
 
-# Do the search
+# Get the API-specific URL
 url = API_INFO[opts.api_version]['url']
+
+# Deal with auth if the API requires it...
 auth_helper = API_INFO[opts.api_version].get('auth')
 if callable(auth_helper):
     request_args['auth'] = auth_helper()
     
+# Perform the search
 response = requests.get(url, **request_args)
-
 if response.status_code == 200:
     print response.text
 else:
